71b7943e169e4051cb374ceb5df076d30e810ff6
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2014-09-25 08:58:44 +0200

    Added first version of bubble graph

--- src/end.js
+++ src/end.js
@@ -1,6 +1,7 @@
   
   grph.line = grph_graph_line;
   grph.map = grph_graph_map;
+  grph.bubble = grph_graph_bubble;
 
   this.grph = grph;
 
---
+++ src/graph_bubble.js
@@ -0,0 +1,181 @@
+
+function grph_graph_bubble() {
+
+  var axes = {
+    'x' : grph_axis_linear(true),
+    'y' : grph_axis_linear(false),
+    'object' : grph_axis_colour(),
+    'colour' : grph_axis_colour(),
+    'column' : grph_axis_split(),
+    'row' : grph_axis_split()
+  };
+  axes.x.required = true;
+  axes.y.required = true;
+  axes.object.required = true;
+  var dispatch = d3.dispatch("mouseover", "mouseout");
+
+  var dummy_ = d3.select("body").append("svg")
+    .attr("class", "bubblegraph dummy")
+    .style("visibility", "invisible");
+  var label_size_ = grph_label_size(dummy_);
+
+  var graph = grph_graph(axes, dispatch, function(g) {
+    function nest_object(d) {
+      return axes.object.variable() ? d[axes.object.variable()] : 1;
+    }
+    function nest_colour(d) {
+      return axes.colour.variable() ? d[axes.colour.variable()] : 1;
+    }
+    function nest_column(d) {
+      return axes.column.variable() ? d[axes.column.variable()] : 1;
+    }
+    function nest_row(d) {
+      return axes.row.variable() ? d[axes.row.variable()] : 1;
+    }
+    // setup axes
+    axes.colour.domain(graph.data(), graph.schema());
+    axes.column.domain(graph.data(), graph.schema());
+    axes.row.domain(graph.data(), graph.schema());
+    // determine number of rows and columns
+    var ncol = axes.column.variable() ? axes.column.ticks().length : 1;
+    var nrow = axes.row.variable() ? axes.row.ticks().length : 1;
+    // get labels and determine their height
+    var vschemax = variable_schema(axes.x.variable(), schema);
+    var xlabel = vschemax.title;
+    var label_height = label_size_.height(xlabel) + settings('label_padding');
+    var vschemay = variable_schema(axes.y.variable(), schema);
+    var ylabel = vschemay.title;
+    // set the width, height end domain of the x- and y-axes. We need some 
+    // iterations for this, as the height of the y-axis depends of the height
+    // of the x-axis, which depends on the labels of the x-axis, which depends
+    // on the width of the x-axis, etc. 
+    var w, h;
+    for (var i = 0; i < 2; ++i) {
+      w = graph.width() - settings('padding')[1] - settings('padding')[3] - 
+        axes.y.width() - label_height;
+      w = (w - (ncol-1)*settings('sep')) / ncol;
+      axes.x.width(w).domain(graph.data(), graph.schema());
+      h = graph.height() - settings('padding')[0] - settings('padding')[2] - 
+        axes.x.height() - label_height;
+      h = (h - (nrow-1)*settings('sep')) / nrow;
+      axes.y.height(h).domain(graph.data(), graph.schema());
+    }
+    var l = axes.y.width() + settings('padding')[1] + label_height;
+    var t  = settings('padding')[2];
+    // draw labels
+    var ycenter = t + 0.5*(graph.height() - settings('padding')[0] - settings('padding')[2] - 
+        axes.x.height() - label_height);
+    var xcenter = l + 0.5*(graph.width() - settings('padding')[1] - settings('padding')[3] - 
+        axes.y.width() - label_height);
+    g.append("text")
+      .attr("x", settings('padding')[1]).attr("y", ycenter)
+      .attr("text-anchor", "middle").text(ylabel)
+      .attr("transform", "rotate(90 " + settings('padding')[1] + " " + ycenter + ")");
+    g.append("text").attr("x", xcenter).attr("y", graph.height()-settings('padding')[0])
+      .attr("text-anchor", "middle").text(xlabel);
+
+
+
+    var d = d3.nest().key(nest_column).key(nest_row).key(nest_colour).entries(graph.data());
+
+    for (i = 0; i < d.length; ++i) {
+      var dj = d[i].values;
+      t  = settings('padding')[2];
+      for (var j = 0; j < dj.length; ++j) {
+        // draw x-axis
+        if (j == (dj.length-1)) {
+          g.append("g").attr("class", "xaxis")
+            .attr("transform", "translate(" + l + "," + (t + h) + ")").call(axes.x);
+        }
+        // draw y-axis
+        if (i === 0) {
+          g.append("g").attr("class", "xaxis")
+            .attr("transform", "translate(" + (l - axes.y.width()) + "," + t + ")")
+            .call(axes.y);
+        }
+        // draw box for graph
+        var gr = g.append("g").attr("class", "graph")
+          .attr("transform", "translate(" + l + "," + t + ")");
+        gr.append("rect").attr("class", "background")
+          .attr("width", w).attr("height", h);
+        // draw grid
+        var xticks = axes.x.ticks();
+        gr.selectAll("line.gridx").data(xticks).enter().append("line")
+          .attr("class", "grid gridx")
+          .attr("x1", axes.x.scale).attr("x2", axes.x.scale)
+          .attr("y1", 0).attr("y2", h);
+        var yticks = axes.y.ticks();
+        gr.selectAll("line.gridy").data(yticks).enter().append("line")
+          .attr("class", "grid gridy")
+          .attr("x1", 0).attr("x2", w)
+          .attr("y1", axes.y.scale).attr("y2", axes.y.scale);
+        // add crosshairs to graph
+        var gcrossh = gr.append("g").classed("crosshairs", true);
+        gcrossh.append("line").classed("hline", true).attr("x1", 0)
+          .attr("y1", 0).attr("x2", axes.x.width()).attr("y2", 0)
+          .style("visibility", "hidden");
+        gcrossh.append("line").classed("vline", true).attr("x1", 0)
+          .attr("y1", 0).attr("x2", 0).attr("y2", axes.y.height())
+          .style("visibility", "hidden");
+        // draw bubbles 
+        var dk = dj[j].values;
+        for (k = 0; k < dk.length; ++k) {
+          var cls = "circle" + k;
+          gr.selectAll("circle.bubble" + k).data(dk[k].values).enter().append("circle")
+            .attr("class", "bubble bubble" + k + " " + axes.colour.scale(dk[k].key))
+            .attr("cx", axes.x.scale).attr("cy", axes.y.scale)
+            .attr("r", settings('point_size'));
+        }
+        // next line
+        t += axes.y.height() + settings('sep');
+      }
+      l += axes.x.width() + settings('sep');
+    }
+
+
+    // add hover events to the lines and points
+    g.selectAll(".colour").on("mouseover", function(d, i) {
+      var variable = axes.colour.variable();
+      var value = variable ? (d.key || d[variable]) : undefined;
+      dispatch.mouseover.call(g, variable, value, d);
+    });
+    g.selectAll(".colour").on("mouseout", function(d, i) {
+      var variable = axes.colour.variable();
+      var value = variable ? (d.key || d[variable]) : undefined;
+      dispatch.mouseout.call(g, variable, value, d);
+    });
+
+  });
+
+
+  // Local event handlers
+  // Highlighting of selected line
+  dispatch.on("mouseover", function(variable, value, d) {
+    if (variable) {
+      var classes = axes.colour.scale("" + value);
+      var regexp = /\bcolour([0-9]+)\b/;
+      var colour = regexp.exec(classes)[0];
+      this.selectAll(".colour").classed("colourlow", true);
+      this.selectAll("." + colour).classed({"colourhigh": true, "colourlow": false});
+    }
+    this.selectAll(".hline").attr("y1", axes.y.scale(d)).attr("y2", axes.y.scale(d))
+      .style("visibility", "visible");
+    this.selectAll(".vline").attr("x1", axes.x.scale(d)).attr("x2", axes.x.scale(d))
+      .style("visibility", "visible");
+  });
+  dispatch.on("mouseout", function(variable, value, d) {
+    this.selectAll(".colour").classed({"colourhigh": false, "colourlow": false});
+    this.selectAll(".hline").style("visibility", "hidden");
+    this.selectAll(".vline").style("visibility", "hidden");
+  });
+  // Show crosshairs when hovering over a point
+  /*dispatch.on("pointover", function(variable, value, d) {
+  });
+  dispatch.on("pointout", function(variable, value, d) {
+    this.selectAll(".hline").style("visibility", "hidden");
+    this.selectAll(".vline").style("visibility", "hidden");
+  });*/
+
+  return graph;
+}
+
