520ef49af6115f4ac1248a79c2f2e16aff4485c0
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2014-11-14 16:54:49 +0100

    Moved code from grph_line to new superclass

--- src/graph_line.js
+++ src/graph_line.js
@@ -11,14 +11,9 @@ function grph_graph_line() {
   axes.x.required = true;
   axes.y.required = true;
   var dispatch = d3.dispatch("mouseover", "mouseout", "pointover", "pointout",
-    "click");
+    "click", "ready");
 
-  var dummy_ = d3.select("body").append("svg")
-    .attr("class", "lineargraph dummy")
-    .style("visibility", "invisible");
-  var label_size_ = grph_label_size(dummy_);
-
-  function graph_panel(g, data) {
+  var graph = grph_generic_graph(axes, dispatch, "line", function(g, data) {
     function nest_colour(d) {
       return axes.colour.variable() ? d[axes.colour.variable()] : 1;
     }
@@ -38,127 +33,28 @@ function grph_graph_line() {
         .attr("cx", axes.x.scale).attr("cy", axes.y.scale)
         .attr("r", settings('point_size'));
     }
-  }
+  });
 
-  var graph = grph_graph(axes, dispatch, function(g) {
-    function nest_column(d) {
-      return axes.column.variable() ? d[axes.column.variable()] : 1;
-    }
-    function nest_row(d) {
-      return axes.row.variable() ? d[axes.row.variable()] : 1;
-    }
-    // setup axes
-    for (var axis in axes) axes[axis].domain(graph.data(), graph.schema());
-    // determine number of rows and columns
-    var ncol = axes.column.variable() ? axes.column.ticks().length : 1;
-    var nrow = axes.row.variable() ? axes.row.ticks().length : 1;
-    // get labels and determine their height
-    var vschemax = variable_schema(axes.x.variable(), schema);
-    var xlabel = vschemax.title;
-    var label_height = label_size_.height(xlabel) + settings('label_padding');
-    var vschemay = variable_schema(axes.y.variable(), schema);
-    var ylabel = vschemay.title;
-    // set the width, height end domain of the x- and y-axes. We need some 
-    // iterations for this, as the height of the y-axis depends of the height
-    // of the x-axis, which depends on the labels of the x-axis, which depends
-    // on the width of the x-axis, etc. 
-    var w, h;
-    for (var i = 0; i < 2; ++i) {
-      w = graph.width() - settings('padding')[1] - settings('padding')[3] - 
-        axes.y.width() - label_height;
-      w = (w - (ncol-1)*settings('sep')) / ncol;
-      axes.x.width(w).domain(graph.data(), graph.schema());
-      h = graph.height() - settings('padding')[0] - settings('padding')[2] - 
-        axes.x.height() - label_height;
-      h = (h - (nrow-1)*settings('sep')) / nrow;
-      axes.y.height(h).domain(graph.data(), graph.schema());
-    }
-    var l = axes.y.width() + settings('padding')[1] + label_height;
-    var t  = settings('padding')[2];
-    // create group containing complete graph
-    g = g.append("g").attr("class", "graph graph-line");
-    // draw labels
-    var ycenter = t + 0.5*(graph.height() - settings('padding')[0] - settings('padding')[2] - 
-        axes.x.height() - label_height);
-    var xcenter = l + 0.5*(graph.width() - settings('padding')[1] - settings('padding')[3] - 
-        axes.y.width() - label_height);
-    g.append("text").attr("class", "label label-y")
-      .attr("x", settings('padding')[1]).attr("y", ycenter)
-      .attr("text-anchor", "middle").text(ylabel)
-      .attr("transform", "rotate(90 " + settings('padding')[1] + " " + ycenter + ")");
-    g.append("text").attr("class", "label label-x")
-      .attr("x", xcenter).attr("y", graph.height()-settings('padding')[0])
-      .attr("text-anchor", "middle").text(xlabel);
-    // create each of the panels
-    var d = d3.nest().key(nest_column).key(nest_row).entries(graph.data());
-    for (i = 0; i < d.length; ++i) {
-      var dj = d[i].values;
-      t  = settings('padding')[2];
-      for (var j = 0; j < dj.length; ++j) {
-        // draw x-axis
-        if (j == (dj.length-1)) {
-          g.append("g").attr("class", "axis axis-x")
-            .attr("transform", "translate(" + l + "," + (t + h) + ")").call(axes.x);
-        }
-        // draw y-axis
-        if (i === 0) {
-          g.append("g").attr("class", "axis axis-y")
-            .attr("transform", "translate(" + (l - axes.y.width()) + "," + t + ")")
-            .call(axes.y);
-        }
-        // draw box for graph
-        var gr = g.append("g").attr("class", "panel")
-          .attr("transform", "translate(" + l + "," + t + ")");
-        gr.append("rect").attr("class", "background")
-          .attr("width", w).attr("height", h);
-        // draw grid
-        var xticks = axes.x.ticks();
-        gr.selectAll("line.gridx").data(xticks).enter().append("line")
-          .attr("class", "grid gridx")
-          .attr("x1", axes.x.scale).attr("x2", axes.x.scale)
-          .attr("y1", 0).attr("y2", h);
-        var yticks = axes.y.ticks();
-        gr.selectAll("line.gridy").data(yticks).enter().append("line")
-          .attr("class", "grid gridy")
-          .attr("x1", 0).attr("x2", w)
-          .attr("y1", axes.y.scale).attr("y2", axes.y.scale);
-        // add crosshairs to graph
-        var gcrossh = gr.append("g").classed("crosshairs", true);
-        gcrossh.append("line").classed("hline", true).attr("x1", 0)
-          .attr("y1", 0).attr("x2", axes.x.width()).attr("y2", 0)
-          .style("visibility", "hidden");
-        gcrossh.append("line").classed("vline", true).attr("x1", 0)
-          .attr("y1", 0).attr("x2", 0).attr("y2", axes.y.height())
-          .style("visibility", "hidden");
-        // draw panel
-        graph_panel(gr, dj[j].values);
-        // next panel
-        t += axes.y.height() + settings('sep');
-      }
-      l += axes.x.width() + settings('sep');
-    }
-
-
+  // when finished drawing graph; add event handlers 
+  dispatch.on("ready", function() {
     // add hover events to the lines and points
-    g.selectAll(".colour").on("mouseover", function(d, i) {
+    var self = this;
+    this.selectAll(".colour").on("mouseover", function(d, i) {
       var variable = axes.colour.variable();
       var value = variable ? (d.key || d[variable]) : undefined;
-      dispatch.mouseover.call(g, variable, value, d);
-      if (!d.key) dispatch.pointover.call(g, variable, value, d);
+      dispatch.mouseover.call(self, variable, value, d);
+      if (!d.key) dispatch.pointover.call(this, variable, value, d);
     }).on("mouseout", function(d, i) {
       var variable = axes.colour.variable();
       var value = variable ? (d.key || d[variable]) : undefined;
-      dispatch.mouseout.call(g, variable, value, d);
-      if (!d.key) dispatch.pointout.call(g, variable, value, d);
+      dispatch.mouseout.call(self, variable, value, d);
+      if (!d.key) dispatch.pointout.call(this, variable, value, d);
     }).on("click", function(d, i) {
       var variable = axes.colour.variable();
       var value = variable ? (d.key || d[variable]) : undefined;
-      dispatch.click.call(g, variable, value, d);
+      dispatch.click.call(self, variable, value, d);
     });
-
   });
-
-
   // Local event handlers
   // Highlighting of selected line
   dispatch.on("mouseover", function(variable, value, d) {
