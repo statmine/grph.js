1a0935db4060936045b52c7455f226f762114fca
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2015-01-21 10:28:10 +0100

    A little bit of clean up

--- src/graph_line.js
+++ src/graph_line.js
@@ -2,8 +2,6 @@
 function grph_graph_line() {
 
   var axes = {
-    //'x' : grph_axis_linear(true),
-    //'x' : grph_axis_period(),
     'x' : grph_axis_switch([grph_axis_linear(true), grph_axis_period()]),
     'y' : grph_axis_linear(false),
     'colour' : grph_axis_colour(),
