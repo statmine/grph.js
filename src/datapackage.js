f0229b0152f41c67cf06b0df0154806f7fa6db65
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2015-06-03 18:22:28 +0200

    Fixed two little jslint warnings

--- src/datapackage.js
+++ src/datapackage.js
@@ -8,87 +8,87 @@ function variable_schema(variable, schema) {
 }
 
 function variable_value_formatter(variable, schema){
-	for (var i = 0; i < schema.fields.length; i++){
-		var field = schema.fields[i];
-	    if (field.name == variable){
-			switch(field.type){
-				case "number":{
-					return number_formatter(field);
-				}
-				case "categorical":{
-					return categorical_formatter(field);
-				}
-				case "string":{
-					return categorical_formatter(field);
-				}				
-				default:{
-					return default_formatter(field);
-				}
-			}
-	    }
-	}
-	return default_formatter();
+  for (var i = 0; i < schema.fields.length; i++){
+    var field = schema.fields[i];
+      if (field.name == variable){
+      switch(field.type){
+        case "number":{
+          return number_formatter(field);
+        }
+        case "categorical":{
+          return categorical_formatter(field);
+        }
+        case "string":{
+          return categorical_formatter(field);
+        }        
+        default:{
+          return default_formatter(field);
+        }
+      }
+      }
+  }
+  return default_formatter();
 }
 // creates a formatter for pretty printing values for a specific field 
 function value_formatter(schema){
-	var formatters = {};
-	for (var i = 0; i < schema.fields.length; i++){
-		var field = schema.fields[i];
-		switch(field.type){
-			case "number":{
-				formatters[field.name] = number_formatter(field);
-				break;
-			}
-			case "categorical":{
-				formatters[field.name] = categorical_formatter(field);
-				break;
-			}
-			case "string":{
-				formatters[field.name] = categorical_formatter(field);
-				break;
-			}
-			default:{
-				formatters[field.name] = default_formatter(field);
-			}
-		}
-	}
+  var formatters = {};
+  for (var i = 0; i < schema.fields.length; i++){
+    var field = schema.fields[i];
+    switch(field.type){
+      case "number":{
+        formatters[field.name] = number_formatter(field);
+        break;
+      }
+      case "categorical":{
+        formatters[field.name] = categorical_formatter(field);
+        break;
+      }
+      case "string":{
+        formatters[field.name] = categorical_formatter(field);
+        break;
+      }
+      default:{
+        formatters[field.name] = default_formatter(field);
+      }
+    }
+  }
 
-	return function(datum, name){
-		return formatters[name](datum[name]);
-	};
+  return function(datum, name){
+    return formatters[name](datum[name]);
+  };
 }
 
 function default_formatter(field){
-	return function(value){
-		return value;
-	};
+  return function(value){
+    return value;
+  };
 }
 
 function categorical_formatter(field){
-	var cat_titles = {};
-	for (var i = 0; i < field.categories.length; i++){
-		var cat = field.categories[i];
-		cat_titles[cat.name] = cat.title || cat.name;
-	}
-	return function(value){
-		return cat_titles[value] || "(" + value + ")";
-	};
+  var cat_titles = {};
+  for (var i = 0; i < field.categories.length; i++){
+    var cat = field.categories[i];
+    cat_titles[cat.name] = cat.title || cat.name;
+  }
+  return function(value){
+    return cat_titles[value] || "(" + value + ")";
+  };
 }
 
-FACTOR = /x ?(\d ?\d*)(.*)/
+FACTOR = /x ?(\d ?\d*)(.*)/;
 
 function number_formatter(field){
-	//TODO use rounding?
-	var unit = field.unit || "";
-	var factor = 1;
-	
-	if (FACTOR.test(unit)){
-		var m = FACTOR.exec(unit);
-		factor = parseInt(m[1].replace(" ", ""))
-		unit = m[2];
-	}
+  //TODO use rounding?
+  var unit = field.unit || "";
+  var factor = 1;
+  
+  if (FACTOR.test(unit)){
+    var m = FACTOR.exec(unit);
+    factor = parseInt(m[1].replace(" ", ""));
+    unit = m[2];
+  }
 
-	return function(value){
-		return (factor*value).toLocaleString() + " " + unit || "-";
-	};
+  return function(value){
+    return (factor*value).toLocaleString() + " " + unit || "-";
+  };
 }
